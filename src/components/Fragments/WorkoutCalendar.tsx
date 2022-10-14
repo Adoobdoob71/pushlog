c519ed8ac71f3a46f6795a425b7e767837fb60f6
Adoobdoob71 <eladelad71@gmail.com>
2022-10-14 15:42:07 +0200

    migartion to local dev complete

--- src/components/Fragments/WorkoutCalendar.tsx
+++ src/components/Fragments/WorkoutCalendar.tsx
@@ -2,6 +2,7 @@ import { FC, MutableRefObject } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { CalendarList } from "react-native-calendars";
 import { styles, theme, sizes } from "utils/styles";
+import { HEIGHT } from 'utils/constants';
 import NavigationBar from "expo-navigation-bar";
 import { Modalize } from "react-native-modalize";
 import { IHandles } from "react-native-modalize/lib/options";
@@ -13,7 +14,7 @@ interface Props {
 
 const WorkoutCalendar: FC<Props> = ({ calendarModalRef }) => {
   const agendaTheme = {
-    calendarBackground: theme.colors.card,
+    calendarBackground: theme.colors.background,
     todayTextColor: theme.colors.primary,
     textSectionTitleColor: theme.colors.primary,
     monthTextColor: theme.colors.border,
@@ -30,8 +31,11 @@ const WorkoutCalendar: FC<Props> = ({ calendarModalRef }) => {
   };
 
   const HeaderComponent = (
-    <View style={[{ paddingTop: sizes.SIZE_8, paddingBottom: sizes.SIZE_6 }]}>
+    <View style={[{ paddingVertical: sizes.SIZE_8 }]}>
       <View style={[styles.rowCenter, { paddingHorizontal: sizes.SIZE_8 }]}>
+        <Text style={stylesheet.bottomSheetTitle} numberOfLines={1}>
+          Workout History
+        </Text>
         <Button
           mode="text"
           onPress={() => calendarModalRef.current.close()}
@@ -47,25 +51,41 @@ const WorkoutCalendar: FC<Props> = ({ calendarModalRef }) => {
       ref={calendarModalRef}
       panGestureComponentEnabled
       withHandle={false}
+      modalHeight={HEIGHT}
       modalStyle={{ backgroundColor: theme.colors.background }}
       HeaderComponent={HeaderComponent}
+      customRenderer={
+        <CalendarList
+          // @ts-ignore
+          theme={agendaTheme}
+          pastScrollRange={1}
+          futureScrollRange={1}
+          onCalendarToggled={(enabled) =>
+            NavigationBar.setBackgroundColorAsync(
+              enabled ? theme.colors.card : theme.colors.background
+            )
+          }
+          onDayPress={null}
+          ListFooterComponent={() => (
+            <View style={{ height: sizes.SIZE_40 }}>
+              <Text>Something bro</Text>
+            </View>
+          )}
+        />
+      }
     >
-      <CalendarList
-        // @ts-ignore
-        theme={agendaTheme}
-        pastScrollRange={1}
-        futureScrollRange={1}
-        onCalendarToggled={(enabled) =>
-          NavigationBar.setBackgroundColorAsync(
-            enabled ? theme.colors.card : theme.colors.background
-          )
-        }
-        onDayPress={null}
-      />
     </Modalize>
   );
 };
 
-const stylesheet = StyleSheet.create({});
+const stylesheet = StyleSheet.create({
+  bottomSheetTitle: {
+    color: theme.colors.primary,
+    fontWeight: "bold",
+    fontSize: sizes.SIZE_24,
+    marginTop: sizes.SIZE_12,
+    marginStart: sizes.SIZE_12,
+  },
+});
 
 export default WorkoutCalendar;
